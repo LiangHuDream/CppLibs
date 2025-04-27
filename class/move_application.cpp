9b54d0fc30b8f5a58cbe67ad1e753b3e78fbfa38
caviar95 <yzj1995@126.com>
2025-04-27 21:01:11 +0800

    move construct

---
+++ class/move_application.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+	A() = default;
+
+	A(int n) {
+		data = new int(n);
+		sz = n;
+	}
+	A(const A& other) {
+		data = new int(other.sz);
+		sz = other.sz;
+
+		copy(other.data, other.data + other.sz, data);
+	}
+
+	A(A&& other) {
+		data = other.data;
+		sz = other.sz;
+
+		other.data = nullptr;
+		other.sz = 0;
+	}
+
+	~A() {
+		delete data;
+	}
+
+	int size() const {
+		return sz;
+	}
+private:
+	int *data;
+	int sz;
+};
+
+int main()
+{
+	A a(8);
+	cout << a.size() << endl;
+	A b = move(a);
+
+	cout << a.size() << endl;
+
+	return 0;
+}
