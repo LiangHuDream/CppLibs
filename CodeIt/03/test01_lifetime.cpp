826a1970209a86312d3248224ac3dff3466050a9
guoliang <648100535@qq.com>
2025-09-05 00:36:35 +0800

    9-4everydaycode1000

---
+++ CodeIt/03/test01_lifetime.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "shape.h"
+
+class result {
+public:
+    result() {
+        puts("result()");
+    }
+    ~result() {
+        puts("~result()");
+    }
+};
+
+result process_shape(const shape& shape1, const shape& shape2) 
+{
+    puts("process_shape()");
+    return result();
+}
+
+int main() {
+    puts("main()");
+    process_shape(circle(), triangle());
+    puts("something else");
+    return 0;
+}
