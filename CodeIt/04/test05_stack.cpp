298d43b00a0963440b019eb75908820db44f3fa2
guoliang <648100535@qq.com>
2025-09-05 22:56:48 +0800

    9-5everyday1000

---
+++ CodeIt/04/test05_stack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>     // std::cout/endl
+#include <stack>        // std::stack
+
+int main()
+{
+    std::stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    while (!s.empty()) {
+        std::cout <<s.top() << std::endl;
+        s.pop();
+    }
+    return 0;
+}
--- CodeIt/common/output_container.h
+++ CodeIt/common/output_container.h
@@ -20,55 +20,97 @@ template <typename T>
 struct has_output_function {
     template <class U>
     static auto output(U* ptr)
-    -> decltype(std::declval<std::ostream&>() << *ptr, 
-               std::true_type());
+        -> decltype(std::declval<std::ostream&>() << *ptr,
+                    std::true_type());
     template <class U>
     static std::false_type output(...);
-    static constexpr bool value = 
+    static constexpr bool value =
         decltype(output<T>(nullptr))::value;
 };
 
 template <typename T>
-inline constexpr bool has_out_function_v = 
+inline constexpr bool has_output_function_v =
     has_output_function<T>::value;
 
+// Output function for std::pair
 template <typename T, typename U>
 std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& pr);
 
 // Element output function for containers that define a key_type and 
 // have its value type as std::pair
 template <typename T, typename Cont>
-auto output_element(std::ostream& os, const T& element, const Cont&, std::true_type)
-    ->decltype(std::declval<typename Cont::key_type>(), os);
+auto output_element(std::ostream& os, const T& element,
+                    const Cont&, std::true_type)
+    -> decltype(std::declval<typename Cont::key_type>(), os);
 // Element output function for other containers
 template <typename T, typename Cont>
-auto output_element(std::ostream& os, const T& element, const Cont&, ...)
-    ->decltype(os);
+auto output_element(std::ostream& os, const T& element,
+                    const Cont&, ...)
+    -> decltype(os);
 
 // Main output function, enabled only if no output function already exists
-template <typename T, 
-            typename = std::enable_if_t<!has_output_function_v<T>>>
+template <typename T,
+          typename = std::enable_if_t<!has_output_function_v<T>>>
 auto operator<<(std::ostream& os, const T& container)
-    ->decltype(container.begin(), container.end(), os)
+    -> decltype(container.begin(), container.end(), os)
 {
     using std::decay_t;
     using std::is_same_v;
 
-    using element_type = decay_t<decltype(* constainer.beign())>;
+    using element_type = decay_t<decltype(*container.begin())>;
     constexpr bool is_char_v = is_same_v<element_type, char>;
     if constexpr (!is_char_v) {
-        os<< "{";
+        os << "{ ";
     }
-    if (!constainer.empty()) {
-
+    if (!container.empty()) {
+        auto end = container.end();
+        bool on_first_element = true;
+        for (auto it = container.begin(); it != end; ++it) {
+            if constexpr (is_char_v) {
+                if (*it == '\0') {
+                    break;
+                }
+            }
+            if constexpr (!is_char_v) {
+                if (!on_first_element) {
+                    os << ", ";
+                } else {
+                    on_first_element = false;
+                }
+            }
+            output_element(os, *it, container, is_pair<element_type>());
+        }
     }
-
-}
-template <typename T, typename Cont>
-{
-    oss << "(" << pr.first << "," << pr.second << ")";
+    if constexpr (!is_char_v) {
+        os << " }";
+    }
     return os;
 }
+
+template <typename T, typename Cont>
+auto output_element(std::ostream& os, const T& element, const Cont&, std::true_type)
+    ->decltype(std::declval<typename Cont::key_type>(), os)
+{
+    os << element.first << "=>" << element.second;
+    return os;
+}
+
+template <typename T, typename Cont>
+auto output_element(std::ostream& os, const T& element, 
+                    const Cont&, ...)
+    ->decltype(os)
+{
+    os << element;
+    return os;
+}
+
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& pr)
+{
+    os << '(' << pr.first << ", " << pr.second << ')';
+    return os;
+}
+
 
 
 #endif // OUTPUT_CONTAINER_H
